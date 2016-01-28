eae7e4d1b4be463c2ab0c9e8077d3fa73b27cbbe
Richard Hsu <rhsu@gwu.edu>
2016-01-27 23:27:35 -0500

    Added resource provider for search

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,7 +1,15 @@
-var app = angular.module('searchFunction', []);
+var app = angular.module('searchFunction', ['ngResource']);
 
-app.controller('MainCtrl', ['$scope', function($scope) {
-    $scope.name = "World";
+app.controller('MainCtrl', ['$scope', '$resource', function($scope, $resource) {
+
+    $scope.query= $resource("http://api.geonames.org/searchJSON?q=dc&maxRows=10&username=rhsu0268");
+
+
+    $scope.getData = function()
+    {
+        $scope.queryResult = $scope.query.get();
+    }
+
 
 
 
