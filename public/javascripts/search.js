4869fc33685fa511295cce8683ba2b4456845ab3
Richard Hsu <rhsu@gwu.edu>
2016-01-28 23:45:06 -0500

    Added state parameter for the search

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -29,45 +29,50 @@ app.factory('searchResults', ['$http', function($http) {
 
 app.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 
+    $scope.error = false;
 
 
     $scope.getData = function()
     {
+        $scope.error = false;
         var place = $scope.place;
+        var state = $scope.state;
+
         var encodePlace = encodeURI(place);
+        var query;
 
-        // build the query string
-        var query = 'http://api.geonames.org/postalCodeLookupJSON?placename=' + encodePlace + '&country=US' + '&username=rhsu0268';
-        console.log(query);
+        if (state)
+        {
+            query = 'http://api.geonames.org/searchJSON?q=' + encodePlace + '&country=US&adminCode1=' + state + '&username=rhsu0268';
+        }
+        else
+        {
+            // build the query string
+            //var query = 'http://api.geonames.org/postalCodeLookupJSON?placename=' + encodePlace + '&country=US' + '&username=rhsu0268';
+            query = 'http://api.geonames.org/searchJSON?q=' + encodePlace + '&country=US' + '&username=rhsu0268';
+            console.log(query);
 
-
+        }
+        
         $http({method: 'GET', url: query})
             .success(function(data, status)
             {
-                $scope.queryResult = data.postalcodes;
-                console.log(data.postalcodes);
-                /*
-                for (var i = 0; i < $scope.queryResult.length; i++)
+                console.log(data);
+                if (data.geonames.length == 0)
                 {
-                    console.log($scope.queryResult[i]);
+                    $scope.error = true;
+                    console.log("Sorry, your search yields no results");
+                    return;
                 }
-                */
+                $scope.queryResult = data.geonames;
+                console.log($scope.queryResult);
 
 
-                var areaArray = [];
-                for (var area in $scope.queryResult.postalcodes)
-                {
-                    var tempObj = $scope.queryResult.postalcodes[area];
-                    areaArray.push(tempObj);
-                }
-
-                console.log(areaArray);
 
             })
             .error(function(data, status) {
                 alert("Error");
             });
-
 
     }
 
