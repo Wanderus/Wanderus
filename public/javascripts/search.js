67c6507af1c1e25fe83cdf161cec78a868e3ceca
Richard Hsu <rhsu@gwu.edu>
2016-01-29 19:58:00 -0500

    Changed centering on the leaflet map

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -31,7 +31,7 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 
     $scope.error = false;
 
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([38.907347, -77.036591], 13);
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
 			maxZoom: 18,
