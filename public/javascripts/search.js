e900effc2015f03f4be13529d8c4e57691574750
Richard Hsu <rhsu@gwu.edu>
2016-04-02 14:28:25 -0400

    Fixed broken map init; Replaced searches with placenames

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -276,45 +276,13 @@ app.controller('MyController', ['$scope', 'userQuery', function ($scope, userQue
   });
 
 
-  var dishes = [
-    'noodles',
-    'sausage',
-    'beans on toast',
-    'cheeseburger',
-    'battered mars bar',
-    'crisp butty',
-    'yorkshire pudding',
-    'wiener schnitzel',
-    'sauerkraut mit ei',
-    'salad',
-    'onion soup',
-    'bak choi',
-    'avacado maki'
-  ];
 
-  var sides = [
-    'with chips',
-    'a la king',
-    'drizzled with cheese sauce',
-    'with a side salad',
-    'on toast',
-    'with ketchup',
-    'on a bed of cabbage',
-    'wrapped in streaky bacon',
-    'on a stick with cheese',
-    'in pitta bread'
-  ];
 
-  var data = userQuery.getData();
-  console.log(typeof(data));
-  $scope.meals = data.userQuery;
-  console.log($scope.meals);
-  for (var i = 1; i <= 100; i++) {
-    var dish = dishes[Math.floor(Math.random() * dishes.length)];
-    var side = sides[Math.floor(Math.random() * sides.length)];
-    //$scope.meals.push('meal ' + i + ': ' + dish + ' ' + side);
-    //$scope.meals.push(data);
-  }
+  //var data = userQuery.getData();
+  //console.log(typeof(data));
+  //$scope.places = data.userQuery;
+  //console.log($scope.places);
+
 
   //console.log($scope.meals);
 
