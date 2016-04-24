dbbb6f9f2003cbc38cec2d80d2f9015d27c2b016
Richard Hsu <rhsu@gwu.edu>
2016-04-23 21:00:45 -0400

    Replaced map

--- public/javascripts/search2.js
+++ public/javascripts/search2.js
@@ -51,6 +51,11 @@ app.controller('MainCtrl', ['$scope', '$http', '$state', 'search', function($sco
     //$scope.search = search;
 	// test it out
     // test it out
+
+    // init map
+    initMap();
+
+
     $scope.getData = function()
     {
         //var a = {};
@@ -78,6 +83,19 @@ app.controller('MainCtrl', ['$scope', '$http', '$state', 'search', function($sco
 
 
 }]);
+
+function initMap()
+{
+    var map = L.map('map').setView([38.907347, -77.036591], 13);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+			id: 'mapbox.streets'
+	}).addTo(map);
+}
 
 
 /*
