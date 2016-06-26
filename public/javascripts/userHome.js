f3159711a95c5d2a79f1dbe030edf6536aca9276
Richard Hsu <rhsu0268@gmail.com>
2016-06-26 11:47:31 -0400

    Hiding routes

---
+++ public/javascripts/userHome.js
@@ -0,0 +1,7 @@
+var app = angular.module('userHome', ['ui.router']);
+
+
+app.controller('userHomeCtrl', ['$scope', '$state', '$window', function($scope, $state, $window)
+{
+    console.log("userHomeCtrl");
+}]);
