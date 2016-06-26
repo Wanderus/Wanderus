b3bc72c2bce1645425fd44eda6a41696324b0f1d
Richard Hsu <rhsu0268@gmail.com>
2016-06-25 22:17:00 -0400

    Added login and register routes

--- routes/index.js
+++ routes/index.js
@@ -40,7 +40,7 @@ router.get('/searchPart2', function(req, res, next) {
 
 router.get('/result/:id', function(req, res, next) {
 
-  
+
     var id = req.params.id;
 
     Location.find({_id: id}, function(err, result)
@@ -306,6 +306,57 @@ router.get('/deleteLocations', function(req, res, next) {
     });
         res.send("You have successfully removed all your data");
 
+
+});
+
+
+// logging in and logging out
+// logging and logging out
+router.post('/register', function(req, res, next) {
+
+    if (!req.body.username || !req.body.password )
+    {
+        return res.status(400).json({message: 'Please fill out all fields'});
+    }
+    var user = new User();
+
+    user.username = req.body.username;
+
+    user.setPassword(req.body.password);
+
+    user.save(function(err) {
+        if (err)
+        {
+            return next(err);
+        }
+        return res.json({token: user.generateJWT()});
+    });
+});
+
+router.post('/login', function(req, res, next) {
+    if (!req.body.username || !req.body.password)
+    {
+        return res.status(400).json({message: 'Please fill out all fields'});
+    }
+
+    passport.authenticate('local', function(err, user, info) {
+
+        if (err)
+        {
+            return next(err);
+        }
+
+        if (user)
+        {
+            return res.json({token: user.generateJWT()});
+        }
+        else
+        {
+            return res.status(401).json(info);
+        }
+
+
+    })(req, res, next);
 
 });
 
