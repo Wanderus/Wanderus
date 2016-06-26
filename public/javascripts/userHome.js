40aae99a436bd314a469ab1692f073d24759ff67
Richard Hsu <rhsu0268@gmail.com>
2016-06-26 12:13:15 -0400

    Took out login and register packages

--- public/javascripts/userHome.js
+++ public/javascripts/userHome.js
@@ -1,7 +1,73 @@
 var app = angular.module('userHome', ['ui.router']);
 
 
-app.controller('userHomeCtrl', ['$scope', '$state', '$window', function($scope, $state, $window)
+app.controller('userHomeCtrl', ['$scope', '$state', '$window', 'auth', function($scope, $state, $window, auth)
 {
     console.log("userHomeCtrl");
+}]);
+
+app.factory('auth', ['$http', '$window', function($http, $window) {
+
+    var auth = {};
+
+    auth.saveToken = function(token)
+    {
+        $window.localStorage['GETfit-token'] = token;
+    };
+
+    auth.getToken = function()
+    {
+        return $window.localStorage['GETfit-token'];
+    };
+
+    auth.isLoggedIn = function()
+    {
+        var token = auth.getToken();
+
+        if (token)
+        {
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+            return payload.exp > Date.now() / 1000;
+        }
+        else
+        {
+            return false;
+        }
+    };
+
+    auth.currentUser = function()
+    {
+        if (auth.isLoggedIn())
+        {
+            var token = auth.getToken();
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+            return payload.username;
+        }
+    };
+
+    auth.register = function(user)
+    {
+        return $http.post('/register', user).success(function(data) {
+
+            auth.saveToken(data.token);
+
+        });
+    };
+
+    auth.logIn = function(user)
+    {
+        return $http.post('/login', user).success(function(data) {
+
+            auth.saveToken(data.token);
+
+        });
+    };
+
+    auth.logOut = function()
+    {
+        $window.localStorage.removeItem('GETfit-token');
+    };
+    return auth;
 }]);
